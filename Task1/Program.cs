56f9039117c124993868998c749d8adc86331a72
Barotov Mustafo <barotov2075@gmail.com>
2023-12-21 16:13:13 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿class Teacher
+{
+    string fullName;
+    string subject;
+    int experience;
+
+    public Teacher() 
+    {
+
+    }
+    public Teacher(string name, string subjec)
+    {
+        fullName = name;
+
+        subject = subjec;
+    }
+    public Teacher(string name, string subjec, int experience)
+    {
+        fullName = name;
+
+        subject = subjec;
+
+        this.experience = experience;
+    }
+    public string Teach()
+    {
+        return $"{fullName} is teaching {subject}";
+    }
+    public void SetExperience(int years)
+    {
+        experience = years;
+    }
+    public int GetExperience()
+    {
+        return experience;
+    }
+}
